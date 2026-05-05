25bbac825d747fe15341fb13302ab0b4422d6fbb
developerdevanshh <vermadev8749@gmail.com>
2026-05-05 11:37:27 +0530

    first code

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args){ 
+        // byte b = 127; // byte can hold values from -128 to 127  
+        // int a = 555; // int can hold values from -2,147,483,648 to 2,147,483,647
+        // byte k = (byte)a;
+
+        float f = 5.65f;
+        int t = (int)f;
+        System.out.println(f);
+        System.out.println(t);
+
+    }
+}
