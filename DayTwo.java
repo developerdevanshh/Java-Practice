58d17d2db704e2d58036287e1a6933476a3cd11b
developerdevanshh <vermadev8749@gmail.com>
2026-05-06 15:03:40 +0530

    Day Two

--- DayTwo.java
+++ DayTwo.java
@@ -20,13 +20,53 @@ public class DayTwo{
         // }
 
         // Leap year checker
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a year:");
-        int year = sc.nextInt();
-        if(year % 4 == 0){
-            System.out.println(year + " is a leap year.");
-        } else {
-            System.out.println(year + " is not a leap year.");
-        }
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a year:");
+        // int year = sc.nextInt();
+        // if(year % 4 == 0){
+        //     System.out.println(year + " is a leap year.");
+        // } else {
+        //     System.out.println(year + " is not a leap year.");
+        // }
+
+        // Prime Numbers
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a positive integer: ");
+        // int n = sc.nextInt();
+        // boolean isPrime = true;
+        
+        // for(int i = 2;i<=Math.sqrt(n);i++){
+        //     if(n % i == 0){
+        //         isPrime = false;
+        //         break;
+        //     }
+        // }
+        // if(isPrime){
+        //     System.out.println(n + " is a prime number.");
+        // } else {
+        //     System.out.println(n + " is not a prime number.");
+        // }
+
+        // // Multiplication table generator
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a number: ");
+        // int table_num = sc.nextInt();
+
+        // for(int i=1;i<=10;i++){
+        //     System.out.println(table_num + " x " + i + " = " + (table_num * i));
+        // }
+
+        // Factorial calculator
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a positive integer: ");
+        // int num = sc.nextInt();
+        // int fac = 1;
+
+        // for(int i=1;i<=num;i++){
+        //     fac *= i;
+        // }
+        // System.out.println("Factorial of: " + num + " is " + fac);
+
+        
     }
 }
