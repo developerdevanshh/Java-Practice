5a1c6b53d6ed53c92425c8545046bcc3da7c8ca0
developerdevanshh <vermadev8749@gmail.com>
2026-05-08 17:54:22 +0530

    Day Four

--- Animal.java
+++ Animal.java
@@ -5,4 +5,22 @@ public class Animal{
     public void makeSound(){
         System.out.println("Some Sound");
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    // They are for encapsulation
 }
