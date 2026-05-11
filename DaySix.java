6bb8784d0e68e4575d4802f0608ad71e3451241e
developerdevanshh <vermadev8749@gmail.com>
2026-05-11 15:26:25 +0530

    Day Five

--- DaySix.java
+++ DaySix.java
@@ -1,5 +1,15 @@
 public class DaySix{
     public static void main(String[] args){
-        
+        // Sum of N natural numbers
+        int n = sumOfNNaturalNumbers(30);
+        System.out.println(n);
+
+    }
+
+    public static int sumOfNNaturalNumbers(int n){
+        if(n == 1){
+            return n;
+        }
+        return n + sumOfNNaturalNumbers(n-1);
     }
 }
