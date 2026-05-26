6470255b11babe63771e0a8c033913400bb1eb7c
developerdevanshh <vermadev8749@gmail.com>
2026-05-26 19:00:19 +0530

    Interface Practice

--- Practice/src/com/dev/engineer/interfaces/Test.java
+++ Practice/src/com/dev/engineer/interfaces/Test.java
@@ -1,15 +1,25 @@
 package com.dev.engineer.interfaces;
 
+import com.dev.engineer.multipleInheritance.Smartphone;
+
 public class Test {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        Cat cat = new Cat();
-        dog.eat();
-        System.out.println(Animal.MAX_AGE);
-        System.out.println(Dog.MAX_AGE);
-        Animal.info();
-        dog.run();
-        cat.run();
+//        Dog dog = new Dog();
+//        Cat cat = new Cat();
+//        dog.eat();
+//        System.out.println(Animal.MAX_AGE);
+//        System.out.println(Dog.MAX_AGE);
+//        Animal.info();
+//        dog.run();
+//        cat.run();
+
+        Smartphone sp = new Smartphone();
+        sp.makeCall("8764125851");
+        sp.endCall();
+        sp.playMusic();
+        sp.stopMusic();
+        sp.takePhoto();
+        sp.recordVideo();
 
     }
 }
