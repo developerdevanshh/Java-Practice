65bbdbc72bfd7453b4c4005c3c38f546e17055e3
developerdevanshh <vermadev8749@gmail.com>
2026-05-29 14:16:31 +0530

    SOLID Principles Practice

---
+++ Practice/src/com/dev/engineer/solid/I_Interface_Segagration.java
@@ -0,0 +1,47 @@
+package com.dev.engineer.solid;
+
+interface Workable {
+    void work();
+}
+
+interface Eatable {
+    void eat();
+}
+
+interface Sleepable {
+    void sleep();
+}
+
+class Human implements Workable, Eatable, Sleepable {
+    public void work() {
+        System.out.println("Human Works.");
+    }
+
+    public void eat() {
+        System.out.println("Human eats.");
+    }
+
+    public void sleep() {
+        System.out.println("Human Sleeps.");
+    }
+}
+
+class Robot implements Workable {
+    public void work() {
+        System.out.println("Robot Works");
+    }
+}
+public class I_Interface_Segagration {
+    public static void main(String[] args) {
+        
+        Human h = new Human();
+        h.eat();
+        h.work();
+        h.sleep();
+
+        System.out.println();
+
+        Robot r = new Robot();
+        r.work();
+    }
+}
