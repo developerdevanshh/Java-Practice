8a0f49f956c27b0d90fe0a0ffffef7ab166abc9b
developerdevanshh <vermadev8749@gmail.com>
2026-05-29 13:56:39 +0530

    SOLID Principles Practice

--- Practice/src/com/dev/engineer/solid/L_BirdsFly.java
+++ Practice/src/com/dev/engineer/solid/L_BirdsFly.java
@@ -1,4 +1,35 @@
 package com.dev.engineer.solid;
 
+class Bird{
+    void eat(){
+        System.out.println("Bird is eating.");
+    }
+}
+
+interface Flyable {
+    void fly();
+}
+
+class Sparrow extends Bird implements Flyable{
+    public void fly() {
+        System.out.println("Sparrow Flies");
+    }
+}
+
+class Ostrich extends Bird {
+    void run(){
+        System.out.println("Ostrich runs fast");
+    }
+}
+
 public class L_BirdsFly {
+    public static void main(String[] args) {
+        Sparrow sparrow = new Sparrow();
+        sparrow.eat();
+        sparrow.fly();
+
+        Ostrich ostrich = new Ostrich();
+        ostrich.eat();
+        ostrich.run();
+    }
 }
