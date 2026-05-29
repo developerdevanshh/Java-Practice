c946883454d7ff5852dce6fc95dbc826020a45ae
developerdevanshh <vermadev8749@gmail.com>
2026-05-29 22:08:23 +0530

    SOLID Principles Practice

---
+++ Practice/src/com/dev/engineer/Static_InnerClass.java
@@ -0,0 +1,18 @@
+package com.dev.engineer;
+class Outer {
+    static int x = 10;
+
+    // static nested class
+    static class Inner {
+        void display() {
+            System.out.println("x = " + x);
+        }
+    }
+}
+public class Static_InnerClass {
+    public static void main(String[] args) {
+        Outer.Inner obj = new Outer.Inner();
+
+        obj.display();
+    }
+}
