3e8c2f2d995c21fbe56004b55124817b293bdf6d
developerdevanshh <vermadev8749@gmail.com>
2026-05-31 22:43:47 +0530

    Exception Handling Practice

---
+++ Practice/src/com/dev/engineer/exception_handling/CheckedExceptionDemo.java
@@ -0,0 +1,15 @@
+package com.dev.engineer.exception_handling;
+
+import java.io.FileReader;
+
+public class CheckedExceptionDemo {
+    public static void main(String[] args) {
+        try{
+            FileReader file = new FileReader("Add.txt");
+            System.out.println("File Opened Successfully.");
+        } catch (Exception e){
+            System.out.println("Checked Exception Handled.");
+            System.out.println(e);
+        }
+    }
+}
