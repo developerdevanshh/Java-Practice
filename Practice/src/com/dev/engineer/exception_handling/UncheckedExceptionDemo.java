4ae6a439343660d17f65ce133a78274970152729
developerdevanshh <vermadev8749@gmail.com>
2026-05-31 22:46:46 +0530

    Exception Handling Practice

---
+++ Practice/src/com/dev/engineer/exception_handling/UncheckedExceptionDemo.java
@@ -0,0 +1,12 @@
+package com.dev.engineer.exception_handling;
+
+public class UncheckedExceptionDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+
+        int res = a / b;
+
+        System.out.println("Result is : " + res);
+    }
+}
