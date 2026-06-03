6fe334cc6e17aaff92956821a64b530a3e45d962
developerdevanshh <vermadev8749@gmail.com>
2026-06-03 21:13:47 +0530

    Array Practice

---
+++ Practice/src/com/dev/java/practice/arrayquestions/FrequencyCheck.java
@@ -0,0 +1,24 @@
+package com.dev.java.practice.arrayquestions;
+
+import java.util.*;
+public class FrequencyCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Step 1: Read the Comma-separated line of numbers
+        if (!sc.hasNextLine()) returns;
+        String firstLine = sc.nextLine().trim();
+
+        // Split the string by commas and covert to an integer array
+        String[] tokens = firstLine.split(",");
+        int[] arr = new int[tokens.length];
+        for (int i = 0;i < tokens.length; i++){
+            arr[i] = Integer.parseInt(tokens[i].trim());
+        }
+
+        // Read the integer K
+        int k = sc.nextInt();
+
+        // 
+    }
+}
