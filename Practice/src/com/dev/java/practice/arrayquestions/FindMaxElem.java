04131280f5403da83bf0417a8df6d21a7174b661
developerdevanshh <vermadev8749@gmail.com>
2026-06-06 22:29:06 +0530

    Array Practice

--- Practice/src/com/dev/java/practice/arrayquestions/FindMaxElem.java
+++ Practice/src/com/dev/java/practice/arrayquestions/FindMaxElem.java
@@ -1,4 +1,8 @@
 package com.dev.java.practice.arrayquestions;
+
+import java.util.*;
+import java.io.*;
+
 public class FindMaxElem {
 //    //Iterative Approach - O(n) Time and O(1) Space
 //    static int largest(int[] arr){
@@ -24,26 +28,29 @@ public class FindMaxElem {
 //        }
 //    }
 
-    //Recursive Approach - O(n) Time and O(n) Space
-    static int findMax(int[] arr, int i){
+//    //Recursive Approach - O(n) Time and O(n) Space
+//    static int findMax(int[] arr, int i){
+//
+//        // Last index returns the element
+//        if(i == arr.length - 1)
+//            return arr[i];
+//
+//        // Find maximum from the rest of the array
+//        int recMax = findMax(arr,i+1);
+//
+//        // Compare with i-th element and return
+//        return Math.max(recMax, arr[i]);
+//    }
+//
+//    static int largest(int[] arr){
+//        return findMax(arr,0);
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] arr = {10,23,20,15,22,50,85,10,2,22,5};
+//        System.out.println(largest(arr));
+//    }
 
-        // Last index returns the element
-        if(i == arr.length - 1)
-            return arr[i];
+    // Using Library Methods - O(n) Time and O(1) Space
 
-        // Find maximum from the rest of the array
-        int recMax = findMax(arr,i+1);
-
-        // Compare with i-th element and return
-        return Math.max(recMax, arr[i]);
-    }
-
-    static int largest(int[] arr){
-        return findMax(arr,0);
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {10,23,20,15,22,50,85,10,2,22,5};
-        System.out.println(largest(arr));
-    }
 }
