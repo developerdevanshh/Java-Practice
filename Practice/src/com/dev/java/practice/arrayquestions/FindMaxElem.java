eb6da095cc8b930a2f089fd5f14a17bd1e888cb6
developerdevanshh <vermadev8749@gmail.com>
2026-06-06 22:09:23 +0530

    Array Practice

--- Practice/src/com/dev/java/practice/arrayquestions/FindMaxElem.java
+++ Practice/src/com/dev/java/practice/arrayquestions/FindMaxElem.java
@@ -7,14 +7,20 @@ public class FindMaxElem {
         // Traverse array elements from second one
         // compare every element with current max
         for(int i : arr){
-            if(arr[i] > max)
-                max = arr[i];
+//            if(arr[i] > max)
+            if(i > max)
+                max = i;
         }
         return max;
     }
 
     public static void main(String[] args) {
-        int[] arr = {10,23,20,15,22,50,58,10,2,22,5};
-        System.out.println(largest(arr));
+        try {
+//            processFile("data.txt");
+            int[] arr = {10,23,20,15,22,50,58,10,2,22,5};
+            System.out.println(largest(arr));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("File error");
+        }
     }
 }
