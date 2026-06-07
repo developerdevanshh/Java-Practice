d8ea6bedb66c6816e5da7e3f1ed430ef943c8965
developerdevanshh <vermadev8749@gmail.com>
2026-06-07 21:04:58 +0530

    Collections Practice

--- Practice/src/com/dev/java/practice/collections/ArrayListExample.java
+++ Practice/src/com/dev/java/practice/collections/ArrayListExample.java
@@ -1,13 +1,22 @@
 package com.dev.java.practice.collections;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 public class ArrayListExample {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
+//        List<String> list = new ArrayList<>();
         list.add("Java");
         list.add("JavaScript");
         list.add("C++");
         list.add("Java"); // Duplicates Allowed
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("Deva");
+        list2.add("Deva");
+        list2.add("Devansh");
+        list2.add("Dev");
+        
+        list.addAll(list2);
 
         System.out.println(list);
 
