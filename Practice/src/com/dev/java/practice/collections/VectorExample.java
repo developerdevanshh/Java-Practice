cb2664852afc965e1af16d0a0720668305618f51
developerdevanshh <vermadev8749@gmail.com>
2026-06-09 20:55:29 +0530

    Collections Practice

--- Practice/src/com/dev/java/practice/collections/VectorExample.java
+++ Practice/src/com/dev/java/practice/collections/VectorExample.java
@@ -4,12 +4,26 @@ import java.util.Vector;
 
 public class VectorExample {
     public static void main(String[] args) {
-        Vector<String> v = new Vector<>();
+//        Vector<String> v = new Vector<>();
+//        v.add("A");
+//        v.add("B");
+//        v.add("C");
+//        System.out.println(v);
 
-        v.add("A");
-        v.add("B");
-        v.add("C");
+        Vector<Integer> vector = new Vector<>(2); // initial Capactiy = 2
+        System.out.println("Initial Capacity: " + vector.capacity());
 
-        System.out.println(v);
+        // Add Elements to trigger capacity increase
+        vector.add(10);
+        vector.add(20);
+        System.out.println("Capacity after adding 2 elements: " + vector.capacity());
+
+        vector.add(30); // Triggers resize (2 → 4)
+        System.out.println("Capacity after adding 3rd element: " + vector.capacity());
+
+        vector.add(40);
+        vector.add(50); // Triggers resize (4 → 8)
+        System.out.println("Capacity after adding 5 elements: " + vector.capacity());
+
     }
 }
