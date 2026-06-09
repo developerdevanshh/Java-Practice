70589ee7f0668055c56c323e4f8fd7b60653abd0
developerdevanshh <vermadev8749@gmail.com>
2026-06-09 21:02:46 +0530

    Collections Practice

--- Practice/src/com/dev/java/practice/collections/VectorExample.java
+++ Practice/src/com/dev/java/practice/collections/VectorExample.java
@@ -28,26 +28,26 @@ public class VectorExample {
 //        vector.add(50); // Triggers resize (4 → 8)
 //        System.out.println("Capacity after adding 5 elements: " + vector.capacity());
 
-        // Creating a default vector
-        Vector v1 = new Vector();
-
-        // Adding Custom elements using add() method
-        v1.add(1);
-        v1.add(2);
-        v1.add("Dev");
-        v1.add("Verma");
-        v1.add(3);
-
-        System.out.println("V1 " + v1);
-
-        // Creating Generic Vector
-        Vector<Integer> v2 = new Vector<Integer>();
-
-        // Adding custom elements using add() method
-        v2.add(10);
-        v2.add(20);
-        v2.add(30);
-
-        System.out.println("V2 " +v2);
+//        // Creating a default vector
+//        Vector v1 = new Vector();
+//
+//        // Adding Custom elements using add() method
+//        v1.add(1);
+//        v1.add(2);
+//        v1.add("Dev");
+//        v1.add("Verma");
+//        v1.add(3);
+//
+//        System.out.println("V1 " + v1);
+//
+//        // Creating Generic Vector
+//        Vector<Integer> v2 = new Vector<Integer>();
+//
+//        // Adding custom elements using add() method
+//        v2.add(10);
+//        v2.add(20);
+//        v2.add(30);
+//
+//        System.out.println("V2 " +v2);
     }
 }
