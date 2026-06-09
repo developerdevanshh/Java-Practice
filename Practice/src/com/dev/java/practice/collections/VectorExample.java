b0a4e6448f8b639ea0c7ac9d95bcf5e3d269db80
developerdevanshh <vermadev8749@gmail.com>
2026-06-09 20:45:12 +0530

    Collections Practice

---
+++ Practice/src/com/dev/java/practice/collections/VectorExample.java
@@ -0,0 +1,16 @@
+package com.dev.java.practice.collections;
+
+import java.util.Vector;
+
+public class VectorExample {
+    public static void main(String[] args) {
+        Vector<String> v = new Vector<>();
+
+        v.add("A");
+        v.add("B");
+        v.add("C");
+        v.add("D");
+
+        System.out.println(v);
+    }
+}
