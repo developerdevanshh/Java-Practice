50f1b6f645091d127c26823ae494aedbbd4ab20e
developerdevanshh <vermadev8749@gmail.com>
2026-06-09 21:02:09 +0530

    Collections Practice

--- Practice/src/com/dev/java/practice/collections/VectorExample.java
+++ Practice/src/com/dev/java/practice/collections/VectorExample.java
@@ -10,20 +10,44 @@ public class VectorExample {
 //        v.add("C");
 //        System.out.println(v);
 
-        Vector<Integer> vector = new Vector<>(2); // initial Capactiy = 2
-        System.out.println("Initial Capacity: " + vector.capacity());
 
-        // Add Elements to trigger capacity increase
-        vector.add(10);
-        vector.add(20);
-        System.out.println("Capacity after adding 2 elements: " + vector.capacity());
+//        newCapacity = oldCapacity * 2
 
-        vector.add(30); // Triggers resize (2 → 4)
-        System.out.println("Capacity after adding 3rd element: " + vector.capacity());
+//        Vector<Integer> vector = new Vector<>(2); // initial Capactiy = 2
+//        System.out.println("Initial Capacity: " + vector.capacity());
+//
+//        // Add Elements to trigger capacity increase
+//        vector.add(10);
+//        vector.add(20);
+//        System.out.println("Capacity after adding 2 elements: " + vector.capacity());
+//
+//        vector.add(30); // Triggers resize (2 → 4)
+//        System.out.println("Capacity after adding 3rd element: " + vector.capacity());
+//
+//        vector.add(40);
+//        vector.add(50); // Triggers resize (4 → 8)
+//        System.out.println("Capacity after adding 5 elements: " + vector.capacity());
 
-        vector.add(40);
-        vector.add(50); // Triggers resize (4 → 8)
-        System.out.println("Capacity after adding 5 elements: " + vector.capacity());
+        // Creating a default vector
+        Vector v1 = new Vector();
 
+        // Adding Custom elements using add() method
+        v1.add(1);
+        v1.add(2);
+        v1.add("Dev");
+        v1.add("Verma");
+        v1.add(3);
+
+        System.out.println("V1 " + v1);
+
+        // Creating Generic Vector
+        Vector<Integer> v2 = new Vector<Integer>();
+
+        // Adding custom elements using add() method
+        v2.add(10);
+        v2.add(20);
+        v2.add(30);
+
+        System.out.println("V2 " +v2);
     }
 }
