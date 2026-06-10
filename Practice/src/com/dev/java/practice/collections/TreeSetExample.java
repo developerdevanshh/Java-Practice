2f884284c6f525b5bff78c6c762c93139fd70452
developerdevanshh <vermadev8749@gmail.com>
2026-06-10 18:16:57 +0530

    TreeSet Practice

--- Practice/src/com/dev/java/practice/collections/TreeSetExample.java
+++ Practice/src/com/dev/java/practice/collections/TreeSetExample.java
@@ -1,15 +1,25 @@
 package com.dev.java.practice.collections;
+import java.util.NavigableMap;
+import java.util.NavigableSet;
 import java.util.TreeSet;
 import java.util.Set;
 public class TreeSetExample {
     public static void main(String[] args) {
-        TreeSet<Integer> ts = new TreeSet<>();
+//        TreeSet<Integer> ts = new TreeSet<>();
+        NavigableSet<String> ts = new TreeSet<>();
+        ts.add("ab");
+        ts.add("bc");
+        ts.add("cd");
+        ts.add("de");
+        ts.add("ef");
+        ts.add("ef");
+        ts.add("ef");
+        ts.add("fg");
+        ts.add("gh");
+        ts.add("hi");
+        ts.add("ij");
+        ts.add("ij");
 
-        ts.add(10);
-        ts.add(30);
-        ts.add(20);
-        ts.add(30);
-
-        System.out.println(ts);
+        System.out.println("Tree Set is: " + ts);
     }
 }
