2be3567afd5f8bc5442ed2797bfc5a1076ef3b82
developerdevanshh <vermadev8749@gmail.com>
2026-06-10 17:58:05 +0530

    HashSet Practice

--- Practice/src/com/dev/java/practice/collections/HashSetExample.java
+++ Practice/src/com/dev/java/practice/collections/HashSetExample.java
@@ -4,25 +4,51 @@ import java.util.*;
 public class HashSetExample {
     public static void main(String[] args) {
 
-        // Instantiate an Object of HashSet
-        HashSet<Integer> hs = new HashSet<>();
+//        // Instantiate an Object of HashSet
+//        HashSet<Integer> hs = new HashSet<>();
+//
+//        // Adding Elements
+//        hs.add(1);
+//        hs.add(4);
+//        hs.add(9);
+//        hs.add(4);
+//        hs.add(9);
+//
+//        System.out.println("HashSet Size: " + hs.size());
+//        System.out.println("Elements in HashSet: " + hs);
+//
+//        // Removing Elements
+//        hs.remove(1);
+//
+//        System.out.println("HastSet after removing element : " + hs);
+//
+//        // Return false if the element is not present
+//        System.out.println("Remove 1 : " + hs.remove(1));
 
-        // Adding Elements
-        hs.add(1);
-        hs.add(4);
-        hs.add(9);
-        hs.add(4);
-        hs.add(9);
+        HashSet<String> hs = new HashSet<>();
 
-        System.out.println("HashSet Size: " + hs.size());
-        System.out.println("Elements in HashSet: " + hs);
+        hs.add("ab");
+        hs.add("bc");
+        hs.add("cd");
+        hs.add("de");
+        hs.add("ef");
+        hs.add("fg");
+        hs.add("gh");
+        hs.add("hi");
+        hs.add("ij");
 
-        // Removing Elements
-        hs.remove(1);
+        System.out.println("Using Iterator : ");
+        Iterator<String> iterator = hs.iterator();
 
-        System.out.println("HastSet after removing element : " + hs);
+        while(iterator.hasNext()){
+            System.out.println(iterator.next() + ", ");
+        }
 
-        // Return false if the element is not present
-        System.out.println("Remove 1 :" + hs.remove(1));
+        System.out.println();
+
+        System.out.println("Using enhanced for loop:");
+        for(String elem : hs){
+            System.out.print(elem + ", ");
+        }
     }
 }
