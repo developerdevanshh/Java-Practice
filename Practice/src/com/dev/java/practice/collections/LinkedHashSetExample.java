a8dd684f94347e363122ee92a819ae2d022d1f66
developerdevanshh <vermadev8749@gmail.com>
2026-06-10 18:09:11 +0530

    LinkedHashSet Practice

--- Practice/src/com/dev/java/practice/collections/LinkedHashSetExample.java
+++ Practice/src/com/dev/java/practice/collections/LinkedHashSetExample.java
@@ -1,12 +1,15 @@
 package com.dev.java.practice.collections;
 
-import java.util.HashSet;
+//import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.Set;
+
 
 public class LinkedHashSetExample {
     public static void main(String[] args) {
-        HashSet<String> lhs = new LinkedHashSet<>();
+        // Creating an empty LinekdhashSet of string type
+        Set<String> lhs = new LinkedHashSet<>();
 
         lhs.add("ab");
         lhs.add("bc");
