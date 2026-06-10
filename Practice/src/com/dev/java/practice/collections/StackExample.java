4bc4b7c5b5165fa475efec2bb9f1b4f1b79c5ccc
developerdevanshh <vermadev8749@gmail.com>
2026-06-10 17:29:53 +0530

    Stack Practice

--- Practice/src/com/dev/java/practice/collections/StackExample.java
+++ Practice/src/com/dev/java/practice/collections/StackExample.java
@@ -4,48 +4,50 @@ import java.io.*;
 import java.util.*;
 public class StackExample {
     public static void main(String[] args) {
-//        // Create a new stack
-//        Stack<Integer> s = new Stack<>();
+//        //        // Create a new stack
+//        //        Stack<Integer> s = new Stack<>();
+//        //
+//        //        // Push Elements onto the stack
+//        //        s.push(1);
+//        //        s.push(2);
+//        //        s.push(3);
+//        //        s.push(4);
+//        //
+//        //        // Pop elements from the stack
+//        //        while (!s.isEmpty()){
+//        //            System.out.println(s.pop());
+//        //        }
 //
-//        // Push Elements onto the stack
-//        s.push(1);
-//        s.push(2);
-//        s.push(3);
-//        s.push(4);
+//                // Default Initialization of Stack
+//        //        Stack stack1 = new Stack();
 //
-//        // Pop elements from the stack
-//        while (!s.isEmpty()){
-//            System.out.println(s.pop());
-//        }
+//        // Initialization of Stack using Generics
+//        Stack<String> stack = new Stack<String>();
+//
+//        // pushing the elements
+//        stack.push("4");
+//        stack.push("All");
+//        stack.push("Geeks");
+//        stack.push("Devs");
+//
+//        //        stack2.push("I");
+//        //        stack2.push("am");
+//        //        stack2.push("from");
+//        //        stack2.push("Kota.");
+//
+//        // Displaying the Stack
+//        System.out.println("Initial Stack: " + stack);
+//        // System.out.println(stack2);
+//
+//        // Removing elements using pop() method
+//        System.out.println("Popped Element:" + stack.pop());
+//        System.out.println("Popped Element:" + stack.pop());
+//        stack.pop();
+//
+//        System.out.println("The element at the Top of the stack is: " + stack.peek());
+//
+//        System.out.println("Final Stack: " + stack);
 
-        // Default Initialization of Stack
-//        Stack stack1 = new Stack();
-
-        // Initialization of Stack using Generics
-        Stack<String> stack = new Stack<String>();
-
-        // pushing the elements
-        stack.push("4");
-        stack.push("All");
-        stack.push("Geeks");
-        stack.push("Devs");
-
-//        stack2.push("I");
-//        stack2.push("am");
-//        stack2.push("from");
-//        stack2.push("Kota.");
-
-        // Displaying the Stack
-        System.out.println("Initial Stack: " + stack);
-//        System.out.println(stack2);
-
-        // Removing elements using pop() method
-        System.out.println("Popped Element:" + stack.pop());
-        System.out.println("Popped Element:" + stack.pop());
-
-        System.out.println("The element at the Top of the stack is: " + stack.peek());
-
-        System.out.println("Final Stack: " + stack);
 
 
     }
