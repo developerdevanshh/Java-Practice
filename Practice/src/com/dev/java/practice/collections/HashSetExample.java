4375e41d6b51ed98ff5fdcb8740d694182f0cc7e
developerdevanshh <vermadev8749@gmail.com>
2026-06-10 17:45:08 +0530

    HashSet Practice

---
+++ Practice/src/com/dev/java/practice/collections/HashSetExample.java
@@ -0,0 +1,17 @@
+package com.dev.java.practice.collections;
+import java.util.*;
+public class HashSetExample {
+    public static void main(String[] args) {
+
+        // Instantiate an Object of HashSet
+        HashSet<Integer> hs = new HashSet<>();
+
+        // Adding Elements
+        hs.add(1);
+        hs.add(4);
+        hs.add(9);
+
+        System.out.println("HashSet Size: " + hs.size());
+        System.out.println("Elements in HashSet: " + hs);
+    }
+}
