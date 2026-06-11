1bcb4b42bcf8913ef50bde7fd3bf414ccb863a49
developerdevanshh <vermadev8749@gmail.com>
2026-06-11 20:11:48 +0530

    HashMap Practice

--- Practice/src/com/dev/java/practice/collections/HashMapExample.java
+++ Practice/src/com/dev/java/practice/collections/HashMapExample.java
@@ -9,14 +9,23 @@ public class HashMapExample {
         // Create a HashMap (No need to mention the Generic type twice)
         HashMap<String,Integer> hashMap = new HashMap<>();
 
+        // Initialization of a HashMap using Generics
+        HashMap<Integer,String> hashMap1 = new HashMap<Integer,String>();
+
         // Add elements to the HashMap
         hashMap.put("Dev",23);
         hashMap.put("Naveen",24);
         hashMap.put("Rahul",22);
 
+        hashMap1.put(1,"Nikhil");
+        hashMap1.put(2,"Aman");
+        hashMap1.put(3,"Harsh");
+
         // Iterate through the HashMap
         for(Map.Entry<String,Integer> entry :hashMap.entrySet()){
             System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
+
+        System.out.println("HashMap2" + hashMap1);
     }
 }
