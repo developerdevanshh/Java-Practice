68e00d52a282fc7d1a35dc0f904a5e4f3bb259e5
developerdevanshh <vermadev8749@gmail.com>
2026-06-12 21:08:21 +0530

    LinkedHashMap Practice

---
+++ Practice/src/com/dev/java/practice/collections/LinkedHashMapExample.java
@@ -0,0 +1,18 @@
+package com.dev.java.practice.collections;
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashMapExample {
+    public static void main(String[] args) {
+        // Using LinkedHashMap of Strings(Keys) and Integer(Values)
+        LinkedHashMap<String,Integer> lhm = new LinkedHashMap<>();
+
+        lhm.put("Apple",15);
+        lhm.put("Bananas",15);
+        lhm.put("BlueBerry",15);
+        lhm.put("Orange",15);
+        lhm.put("Watermelon",15);
+
+
+    }
+}
