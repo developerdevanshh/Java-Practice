5d3ad43bf94b3b23b6e5bde167210aeae796df95
developerdevanshh <vermadev8749@gmail.com>
2026-06-12 20:08:53 +0530

    TreeMap Practice

---
+++ Practice/src/com/dev/java/practice/collections/TreeMapExample.java
@@ -0,0 +1,13 @@
+package com.dev.java.practice.collections;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapExample {
+    public static void main(String[] args) {
+        // Create a TreeMap of Strings (keys) and Integer (values)
+        TreeMap<String, Integer> tm = new TreeMap<>();
+
+        System.out.println(tm);
+    }
+}
