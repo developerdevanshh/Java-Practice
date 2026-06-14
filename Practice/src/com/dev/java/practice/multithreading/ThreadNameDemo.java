95cdacc069920ab7575acadac4ee95341d813f5c
developerdevanshh <vermadev8749@gmail.com>
2026-06-14 19:39:49 +0530

    MutltiThreading Practice

--- Practice/src/com/dev/java/practice/multithreading/ThreadNameDemo.java
+++ Practice/src/com/dev/java/practice/multithreading/ThreadNameDemo.java
@@ -3,6 +3,8 @@ package com.dev.java.practice.multithreading;
 public class ThreadNameDemo extends Thread {
     public void run(){
         System.out.println("Thread Tasks.");
+        String s = Thread.currentThread().getName();
+        System.out.println(s);
     }
     public static void main(String[] args) {
 //        System.out.println("Hello");
@@ -11,9 +13,9 @@ public class ThreadNameDemo extends Thread {
 //        Thread.currentThread().setName("Dev");
 //        String d = Thread.currentThread().getName();
 //        System.out.println(d);
-        System.out.println(0/10);
-//        ThreadNameDemo tnd = new ThreadNameDemo();
-//        Thread t = new Thread(tnd);
-//        t.start();
+//        System.out.println(0/10);
+        ThreadNameDemo tnd = new ThreadNameDemo();
+        Thread t = new Thread(tnd);
+        t.start();
     }
 }
