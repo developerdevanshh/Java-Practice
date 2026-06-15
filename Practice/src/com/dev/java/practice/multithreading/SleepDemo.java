d6b6c81fc9058aa4963d6ad676311e6fce4eb9c0
developerdevanshh <vermadev8749@gmail.com>
2026-06-15 17:14:26 +0530

    MutltiThreading Practice

--- Practice/src/com/dev/java/practice/multithreading/SleepDemo.java
+++ Practice/src/com/dev/java/practice/multithreading/SleepDemo.java
@@ -2,14 +2,14 @@ package com.dev.java.practice.multithreading;
 
 public class SleepDemo extends Thread{
     public void run(){
-        try{
-            for(int i = 0; i<= 5;i++){
+//        try{
+            for(int i = 0; i< 5;i++){
                 System.out.println(i + " : " + Thread.currentThread().getName());
-                Thread.sleep(1000);
+//                Thread.sleep(1000);
             }
-        }catch (Exception e){
-            System.out.println(e);
-        }
+//        }catch (Exception e){
+//            System.out.println(e);
+//        }
     }
 
     public static void main(String[] args) {
@@ -17,10 +17,14 @@ public class SleepDemo extends Thread{
 //        Thread t = new Thread(sd);
 //        t.start();
         sd.start();
+        for(int i = 0; i< 5;i++){
+            System.out.println(i + " : " + Thread.currentThread().getName());
+//                Thread.sleep(1000);
+        }
 
-        SleepDemo sd1 = new SleepDemo();
-//        sd1.start();
-        sd1.run();
+//        SleepDemo sd1 = new SleepDemo();
+////        sd1.start();
+//        sd1.run();
     }
 }
 
