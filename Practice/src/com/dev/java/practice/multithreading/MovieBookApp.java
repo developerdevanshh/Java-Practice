f36dc5fbf601f33848451b46523e56a0f93a3d7b
developerdevanshh <vermadev8749@gmail.com>
2026-06-17 23:32:53 +0530

    Thread Practice

--- Practice/src/com/dev/java/practice/multithreading/MovieBookApp.java
+++ Practice/src/com/dev/java/practice/multithreading/MovieBookApp.java
@@ -3,18 +3,23 @@ package com.dev.java.practice.multithreading;
 class TotalEarnings extends Thread {
     int total = 0;
     public void run(){
-        for(int i = 1;i <= 10;i++)
-        {
-            total += 100;
+        synchronized (this){
+            for(int i = 1;i <= 10;i++) {
+                total += 100;
+            }
+            this.notify();
         }
 //        System.out.println(total);
     }
 }
 
 public class MovieBookApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         TotalEarnings te = new TotalEarnings();
         te.start();
-        System.out.println("Total Earnings : " + te.total);
+        synchronized (te){
+            te.wait();
+        }
+//        System.out.println("Total Earnings : " + te.total);
     }
 }
