f041e2164022a7d5e3da548cec8daecbe1e6b5a1
developerdevanshh <vermadev8749@gmail.com>
2026-06-17 22:31:22 +0530

    StaticSynchronizedThread Practice

--- Practice/src/com/dev/java/practice/multithreading/StaticSynchronized.java
+++ Practice/src/com/dev/java/practice/multithreading/StaticSynchronized.java
@@ -7,10 +7,10 @@ class BookTheaterSeat{
         if(total_seats >= seats){
             System.out.println("Seats booked successfully.");
             total_seats = total_seats - seats;
-            System.out.println("Seats left" + total_seats);
+            System.out.println("Seats left : " + total_seats);
         }else{
             System.out.println("Seats cannot be booked.");
-            System.out.println("Seats left" + total_seats);
+            System.out.println("Seats left : "  + total_seats);
         }
     }
 }
