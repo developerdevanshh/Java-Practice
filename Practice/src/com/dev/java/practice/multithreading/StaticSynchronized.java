ee561300f45f93a2a676ea9f17e329802373604b
developerdevanshh <vermadev8749@gmail.com>
2026-06-17 22:19:29 +0530

    StaticSynchronizedThread Practice

--- Practice/src/com/dev/java/practice/multithreading/StaticSynchronized.java
+++ Practice/src/com/dev/java/practice/multithreading/StaticSynchronized.java
@@ -1,5 +1,20 @@
 package com.dev.java.practice.multithreading;
 
+class BookTheaterApp{
+    int total_seats = 20;
+
+    void bookSeat(int seats){
+        if(total_seats >= seats){
+            System.out.println("Seats booked successfully.");
+            total_seats = total_seats - seats;
+            System.out.println("Seats left" + total_seats);
+        }else{
+            System.out.println("Seats cannot be booked.");
+            System.out.println("Seats left" + total_seats);
+        }
+    }
+}
+
 public class StaticSynchronized {
     public static void main(String[] args) {
 
