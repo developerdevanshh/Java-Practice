ad964bbef1f253c3f04611ec7999a75489fef9f8
developerdevanshh <vermadev8749@gmail.com>
2026-06-17 22:39:37 +0530

    StaticSynchronizedThread Practice

--- Practice/src/com/dev/java/practice/multithreading/StaticSynchronized.java
+++ Practice/src/com/dev/java/practice/multithreading/StaticSynchronized.java
@@ -1,11 +1,11 @@
 package com.dev.java.practice.multithreading;
 
 class BookTheaterSeat{
-    int total_seats = 10;
+    int total_seats = 20;
 
     synchronized void bookSeat(int seats){
         if(total_seats >= seats){
-            System.out.println("Seats booked successfully.");
+            System.out.println(seats + " Seats booked successfully.");
             total_seats = total_seats - seats;
             System.out.println("Seats left : " + total_seats);
         }else{
@@ -13,6 +13,16 @@ class BookTheaterSeat{
             System.out.println("Seats left : "  + total_seats);
         }
     }
+//     void bookSeat(int seats){
+//        if(total_seats >= seats){
+//            System.out.println(seats + " Seats booked successfully.");
+//            total_seats = total_seats - seats;
+//            System.out.println("Seats left : " + total_seats);
+//        }else{
+//            System.out.println("Seats cannot be booked.");
+//            System.out.println("Seats left : "  + total_seats);
+//        }
+//    }
 }
 
 class MyThread1 extends Thread{
@@ -44,14 +54,14 @@ public class StaticSynchronized {
         MyThread1 mt1 = new MyThread1(a,7);
         mt1.start();
 
-        MyThread2 mt2 = new MyThread2(a, 8);
+        MyThread2 mt2 = new MyThread2(a, 9);
         mt2.start();
 
         // ================================
 
 
         BookTheaterSeat a1 = new BookTheaterSeat();
-        MyThread1 t1 = new MyThread1(a1,7);
+        MyThread1 t1 = new MyThread1(a1,5);
         t1.start();
 
         MyThread2 t2 = new MyThread2(a1, 8);
