6f640d7a1774ea40c9e3e5ade02c7fb2979e0471
developerdevanshh <vermadev8749@gmail.com>
2026-06-22 21:04:48 +0530

    Thread RaceCondition Practice

---
+++ Practice/src/com/dev/java/practice/multithreading/RaceCondition.java
@@ -0,0 +1,35 @@
+package com.dev.java.practice.multithreading;
+
+class Counter {
+    int count = 0;
+
+    void increment(){
+        count++; // Not atomic - Read, Add, Write
+    }
+}
+
+public class RaceCondition {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+
+        Thread thread1 = new Thread(() -> {
+            for(int i=0;i< 10000;i++){
+                counter.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for(int i=0;i< 10000;i++){
+                counter.increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+
+
+        System.out.println("Final Count: " + counter.count);
+    }
+}
