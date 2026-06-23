c1e25fc78fc8a6e7e02230b9ee5b0f82bada792a
developerdevanshh <vermadev8749@gmail.com>
2026-06-23 13:42:50 +0530

    Thread Practice

---
+++ Practice/src/com/dev/java/practice/multithreading/VolatileNotEnough.java
@@ -0,0 +1,27 @@
+package Practice.src.com.dev.java.practice.multithreading;
+
+public class VolatileNotEnough {
+    volatile int count = 0;
+    void increment(){
+        count++; // volatile but still not atomic!
+    }
+    public static void main(String[] args) throws InterruptedException {
+        VolatileNotEnough obj = new VolatileNotEnough();
+
+        Thread t1 = new Thread(() -> {
+            for (int i=0;i<10000;i++) obj.increment();
+        });
+
+        Thread t2 = new Thread(()-> {
+            for(int i=0;i<10000;i++) obj.increment();
+        });
+
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
+
+        // Still less than 20000 volatile alone not enough
+        System.out.println("Count: " + obj.count);
+    }
+}
