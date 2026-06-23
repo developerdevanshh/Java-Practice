b10267cebed8f0f0f3a3949fadfe314a82f89683
developerdevanshh <vermadev8749@gmail.com>
2026-06-23 13:18:32 +0530

    Thread Practice

---
+++ Practice/src/com/dev/java/practice/multithreading/DirtyReadDemo.java
@@ -0,0 +1,37 @@
+package Practice.src.com.dev.java.practice.multithreading;
+
+class UserProfile {
+    private String firstName = "John";
+    private String lastName = "Doe";
+
+    // Writer - Updates both fields
+    void update(){
+        firstName = "Jane";
+        try {Thread.sleep(1000);} catch (InterruptedException e) { }
+        // Reader reads here - sees "Jane Doe" - inconsistent!
+        lastName = "Smith";
+    }
+
+    // Reader - reads both fields
+    void display(){
+        System.out.println("Name : " + firstName + " " + lastName);
+    }
+}
+
+
+public class DirtyReadDemo {
+    public static void main(String[] args) {
+        UserProfile profile = new UserProfile();
+
+        Thread writer = new Thread( () -> profile.update());
+
+        Thread reader = new Thread(() -> {
+            try { Thread.sleep(50);} catch (InterruptedException e){ }
+            profile.display();
+        });
+
+        writer.start();
+        reader.start();
+
+    }
+}
