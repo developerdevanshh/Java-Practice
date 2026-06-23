e0ab557aa0df59365f139132121c5409ff4626ea
developerdevanshh <vermadev8749@gmail.com>
2026-06-23 13:06:51 +0530

    Thread Practice

--- Practice/src/com/dev/java/practice/multithreading/DeadLockDemo.java
+++ Practice/src/com/dev/java/practice/multithreading/DeadLockDemo.java
@@ -6,49 +6,49 @@ public class DeadLockDemo {
 
     public static void main(String[] args) {
 
-//        Thread t1 = new Thread(()-> {
-//            synchronized (lock1){
-//                System.out.println("T1 acquired lock1, waiting for lock2");
-//                try{ Thread.sleep(100);} catch (InterruptedException e) {}
-//
-//                synchronized (lock2){
-//                    System.out.println("T1 acquired lock2");
-//                }
-//            }
-//        });
-//
-//        Thread t2 = new Thread(()-> {
-//            synchronized (lock2){
-//                System.out.println("T2 acquired lock1, waiting for lock2");
-//                try{ Thread.sleep(100);} catch (InterruptedException e) {}
-//
-//                synchronized (lock1){
-//                    System.out.println("T2 acquired lock2");
-//                }
-//            }
-//        });
-        // Program hangs forever — DEADLOCK
-
-        // Both threads acquire locks in Same Order
         Thread t1 = new Thread(()-> {
             synchronized (lock1){
-                System.out.println("T1 acquired lock1"); // always lock1 first
+                System.out.println("T1 acquired lock1, waiting for lock2");
+                try{ Thread.sleep(100);} catch (InterruptedException e) {}
+
                 synchronized (lock2){
-                    System.out.println("T1 acquired lock2"); // then lock2
+                    System.out.println("T1 acquired lock2");
                 }
             }
         });
 
         Thread t2 = new Thread(()-> {
-            synchronized (lock1){
-                System.out.println("T2 acquired lock1"); // always lock1 first
-                synchronized (lock2){
-                    System.out.println("T2 acquired lock2"); // then lock2
+            synchronized (lock2){
+                System.out.println("T2 acquired lock1, waiting for lock2");
+                try{ Thread.sleep(100);} catch (InterruptedException e) {}
+
+                synchronized (lock1){
+                    System.out.println("T2 acquired lock2");
                 }
             }
         });
+        // Program hangs forever — DEADLOCK
 
-        // Works perfectly — no deadlock
+//        // Both threads acquire locks in Same Order
+//        Thread t1 = new Thread(()-> {
+//            synchronized (lock1){
+//                System.out.println("T1 acquired lock1"); // always lock1 first
+//                synchronized (lock2){
+//                    System.out.println("T1 acquired lock2"); // then lock2
+//                }
+//            }
+//        });
+//
+//        Thread t2 = new Thread(()-> {
+//            synchronized (lock1){
+//                System.out.println("T2 acquired lock1"); // always lock1 first
+//                synchronized (lock2){
+//                    System.out.println("T2 acquired lock2"); // then lock2
+//                }
+//            }
+//        });
+//
+//        // Works perfectly — no deadlock
 
         t1.start();
         t2.start();
