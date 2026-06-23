bbb7d2463b139c10464a1a5dc10d263a260a639e
developerdevanshh <vermadev8749@gmail.com>
2026-06-23 13:37:10 +0530

    Thread Practice

--- Practice/src/com/dev/java/practice/multithreading/VolatileDemo.java
+++ Practice/src/com/dev/java/practice/multithreading/VolatileDemo.java
@@ -1,4 +1,32 @@
 package Practice.src.com.dev.java.practice.multithreading;
 
+class Task {
+    // Without volatile - thread may cache 'running' and forever
+//    boolean running = true;
+    volatile boolean running = true;
+
+    void run(){
+        int count = 0;
+        while(running){
+            count++;
+        }
+        System.out.println("Thread stopped. Count: " + count);
+    }
+
+    void stop(){
+        running = false;
+        System.out.println("Stop signal sent");
+    }
+}
+
 public class VolatileDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Task t = new Task();
+
+        Thread worker = new Thread(()-> t.run());
+        worker.start();
+
+        Thread.sleep(100); // let worker run for 100ms
+        t.stop(); // signal to stop
+    }
 }
